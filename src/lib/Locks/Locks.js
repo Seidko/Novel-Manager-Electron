ad474ffde9141e863daca963d7caea7d911e1c69
Seidko <hidden233@163.com>
2021-11-09 17:09:49 +0800

    Locks.js 完成Lock

--- src/lib/Locks/Locks.js
+++ src/lib/Locks/Locks.js
@@ -1,18 +1,63 @@
-exports.Locks = class Locks {
-  #_locked
-  get locked () {
-    return this.#_locked
-  }
+/* Lock.js
+*  A utils of Synchronization Primitives
+*  Included Lock, Event, Events, Condition, Barrier and BoundedSemaphore
+*/
 
-  set locked (value) {
-    this.#_locked = value
-  }
+// noinspection JSUnusedGlobalSymbols
+const async_hooks = require('async_hooks')
+
+exports.Lock = class Lock {
+  #_locked
+  #_registry
 
   constructor () {
-    this.locked = false
+    this.#_locked = false
+    this.#_registry = []
   }
 
-  acquire (block, timeout) {}
+  acquire () {
+    // TODO: 用async_hooks.triggerAsyncId()实现RLOCK
+    console.log('id: ' + async_hooks.triggerAsyncId())
+    if (this.#_locked) {
+      return new Promise((resolve) => {
+        this.#_registry.push(() => {
+          resolve(null)
+          this.#_locked = true
+        })
+      })
+    } else {
+      this.#_locked = true
+      return Promise.resolve(null)
+    }
+  }
 
-  release () {}
+  release () {
+    this.#_locked = false
+    const TEMP1 = this.#_registry.pop()
+    if (typeof TEMP1 === 'function') {
+      TEMP1()
+    }
+  }
+
+  locked () {
+    return this.#_locked
+  }
+}
+
+exports.Event = class Event {
+  #_event
+  #_registry
+
+  constructor () {
+    this.#_event = false
+    this.#_registry = []
+  }
+
+  wait () {
+    if (this.#_event) {
+      return Promise.resolve(null)
+    } else {
+      return Promise
+    }
+  }
 }
--- test.js
+++ test.js
@@ -1,11 +1,14 @@
 const Locks = require('./src/lib/Locks/Locks')
 
-const lock = new Locks.Locks()
+const lock = new Locks.Lock()
 
 const temp2 = lock.acquire()
 setTimeout(async () => {
   await temp2
   console.log(2, new Date().toString())
+  lock.release()
+  await lock.acquire()
+  console.log(3, new Date().toString())
   lock.release()
 }, 3000)
 
