025990f0245dd7ed5cc6828210afa883203c4e50
Seidko <hidden2333@163.com>
2022-02-20 12:56:25 +0800

    rewrite

---
+++ src/background.ts
@@ -0,0 +1,66 @@
+'use strict'
+import { app, protocol, BrowserWindow } from 'electron'
+import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
+import installExtension, { VUEJS3_DEVTOOLS } from 'electron-devtools-installer'
+const isDevelopment = process.env.NODE_ENV !== 'production'
+
+// global value definition
+let MainWindow
+// end definition
+
+protocol.registerSchemesAsPrivileged([
+  { scheme: 'app', privileges: { secure: true, standard: true } }
+])
+
+async function createWindow () {
+  // Create the browser window.
+  MainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  })
+
+  if (process.env.WEBPACK_DEV_SERVER_URL) {
+    await MainWindow.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
+    if (!process.env.IS_TEST) MainWindow.webContents.openDevTools()
+  } else {
+    createProtocol('app')
+    MainWindow.loadURL('app://./index.html')
+  }
+}
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', async () => {
+  if (BrowserWindow.getAllWindows().length === 0) await createWindow()
+})
+
+app.on('ready', async () => {
+  if (isDevelopment && !process.env.IS_TEST) {
+    try {
+      await installExtension(VUEJS3_DEVTOOLS)
+    } catch (e) {
+      console.error('Vue Devtools failed to install:', e)
+    }
+  }
+  await createWindow()
+})
+
+if (isDevelopment) {
+  if (process.platform === 'win32') {
+    process.on('message', (data) => {
+      if (data === 'graceful-exit') {
+        app.quit()
+      }
+    })
+  } else {
+    process.on('SIGTERM', () => {
+      app.quit()
+    })
+  }
+}
